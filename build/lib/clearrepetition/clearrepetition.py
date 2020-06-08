5fc43639119b29d5c6d22010462b698a7fea7e3b
Kornpob Bhirombhakdi <kbhirombhakdi@altalune.local>
2020-06-08 09:40:56 -0400

    v.1.1.0

--- build/lib/clearrepetition/clearrepetition.py
+++ build/lib/clearrepetition/clearrepetition.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-def clearrepetition(wave,flam):
+def clearrepetition(wave,flam,eflam=None):
     unique = np.unique(wave)
-    wavetmp,flamtmp = [],[]
+    wavetmp,flamtmp,eflamtmp = [],[],[]
     for i in unique:
         m = np.where(wave == i)
         wavetmp.append(np.mean(wave[m]))
         flamtmp.append(np.mean(flam[m]))
-    return np.array(wavetmp),np.array(flamtmp)
+        if len(eflam) > 0:
+            eflamtmp.append(np.sqrt(np.sum(np.power(eflam[m],2))))
+    return np.array(wavetmp),np.array(flamtmp),np.array(eflamtmp)    
